63432c790134b09c97e76ed9fc07aa7cd0765835
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2019-11-11 00:28:59 -0300

    Allow absolute imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import SiteTemplate from './templates/SiteTemplate';
+import SiteTemplate from 'src/templates/SiteTemplate';
 
 const App: React.FC = () => {
   return (
--- src/templates/SiteTemplate/SiteTemplate.tsx
+++ src/templates/SiteTemplate/SiteTemplate.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../../routes/Home';
+import Home from 'src/routes/Home';
 
 const SiteTemplate: React.FC = () => (
   <>
