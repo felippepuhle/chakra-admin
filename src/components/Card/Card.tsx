723ef6c480c428539e621b548e1a06f38b8d66ef
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2019-11-18 10:07:59 -0300

    Add dark mode on cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { Box, BoxProps } from '@chakra-ui/core';
+import React, { useMemo } from 'react';
+import { Box, BoxProps, useColorMode } from '@chakra-ui/core';
 
-const Card: React.FC<BoxProps> = props => (
-  <Box bg="white" padding={4} borderRadius={4} borderWidth={1} {...props} />
-);
+const Card: React.FC<BoxProps> = props => {
+  const { colorMode } = useColorMode();
+
+  const bg = useMemo(() => (colorMode === 'dark' ? 'gray.700' : 'white'), [
+    colorMode,
+  ]);
+
+  return (
+    <Box bg={bg} padding={4} borderRadius={4} borderWidth={1} {...props} />
+  );
+};
 
 export default Card;
